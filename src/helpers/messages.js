98946b1e9162081aada10559aa2137ea960c3ae7
aflores852023 <aflores85@gmail.com>
2024-08-17 19:19:21 -0300

    modif Message CSS

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -6,7 +6,6 @@ const messages = JSON.parse(localStorage.getItem('messages'));
  * @return {Array} Un arreglo de mensajes filtrados y mapeados según el ID del canal.
  */
 export const getMessagesForChannel = (channelId) => {
-  console.log(messages);
   return messages.filter(message => message.channelId === channelId)
     .map(message => ({...message,timestamp: new Date(message.timestamp),
     }))
