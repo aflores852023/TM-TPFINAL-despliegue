c64f7fe2186b7f1bcabe0f948cf9bef579c396c4
aflores852023 <aflores85@gmail.com>
2024-11-23 15:48:08 -0300

    correcion de rutas para elenviroment.js

--- src/screems/Login/Login.jsx
+++ src/screems/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { extractFormData } from '../../utils/extractFormData'
 import { POST, getUnnauthenticatedHeaders } from '../../fetching/http.fetching'
-import { ENVIROMENT } from '../../enviroment/enviroment'
+import { ENVIROMENT } from '../../../enviroment.js'
 const Login = () => {	
 	const navigate = useNavigate()
 
