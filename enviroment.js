0e57cc35ab3f2ffb4cc785feb8098950da784818
aflores852023 <aflores85@gmail.com>
2024-12-09 21:36:35 -0300

    CONFG VERCEL

--- enviroment.js
+++ enviroment.js
@@ -1,6 +1,7 @@
 const ENVIROMENT = {
-    URL_BACKEND: "https://tm-tpfinal-backend-despliegue.vercel.app",
-    //URL_BACKEND: "http://localhost:3000",
+    URL_BACKEND: process.env.NODE_ENV === 'production'
+        ? 'https://tm-tpfinal-backend-despliegue.vercel.app'
+        : 'http://localhost:3000',
 };
 
 export default ENVIROMENT
